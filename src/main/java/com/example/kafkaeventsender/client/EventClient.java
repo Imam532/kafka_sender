14a48171ca1bd392d780cc669af8362c75ab9d02
Sharabdinov Imamdin <imamdin.sh@inuigaming.com>
2023-05-08 09:30:36 +0300

    statistic client fix

--- src/main/java/com/example/kafkaeventsender/client/EventClient.java
+++ src/main/java/com/example/kafkaeventsender/client/EventClient.java
@@ -10,10 +10,14 @@ import com.inuigaming.inuieventstarter.kafka.MessageService;
 import inui.models.statistic.GameEvent;
 import io.micrometer.core.instrument.util.IOUtils;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.Disposable;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
+import java.time.Duration;
 import java.util.Collections;
 import java.util.List;
 
@@ -21,27 +25,28 @@ import static java.lang.ClassLoader.getSystemResourceAsStream;
 import static java.nio.charset.StandardCharsets.UTF_8;
 import static java.util.Objects.requireNonNull;
 
-
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class EventClient {
 
     private final MessageService messageService;
     private final KafkaMessageCreator kafkaMessageCreator;
-//    private final KafkaEventSender sender;
+    //    private final KafkaEventSender sender;
     private final WebClient webClient;
 
     public void sendMessages(String file) {
-        webClient
-                .post()
-                .uri("/api/v1/statistics")
-                .body(getMessage(file), EventMessage.class)
-                .retrieve()
-                .bodyToMono(String.class)
-                .subscribe(System.out::println);
+        this.getMessage(file);
+//        webClient
+//                .post()
+//                .uri("/api/v1/statistics")
+//                .body(getMessage(file), EventMessage.class)
+//                .retrieve()
+//                .bodyToMono(String.class)
+//                .subscribe(System.out::println);
     }
 
-    private Flux<EventMessage> getMessage(String file) {
+    private void getMessage(String file) {
         try {
             ObjectMapper objectMapper = new ObjectMapper();
             String s = IOUtils.toString(
@@ -50,16 +55,36 @@ public class EventClient {
             var gameEvents = JsonConverter.objectFromJson(s, new TypeReference<List<GameEvent>>() {
             });
             Collections.reverse(gameEvents);
-            return Flux.fromIterable(gameEvents)
+            var messages = gameEvents.stream()
                     .map(gameEvent -> {
                         try {
                             return new EventMessage().setEvent(objectMapper.writeValueAsString(gameEvent));
                         } catch (JsonProcessingException e) {
                             return new EventMessage().setEvent("{}");
                         }
-                    });
+                    })
+                    .map(Mono::just)
+//                    .map(message -> webClient
+//                            .post()
+//                            .uri("/api/v1/statistics")
+//                            .body(message, EventMessage.class)
+//                            .retrieve()
+//                            .bodyToMono(String.class)
+//                            .subscribe())
+                    .toList();
+            log.info("resp {}", messages);
+
+            Flux.fromIterable(messages)
+//                    .delayElements(Duration.ofMillis(100))
+                    .flatMap(message -> webClient
+                            .post()
+                            .uri("/api/v1/statistics")
+                            .body(message, EventMessage.class)
+                            .retrieve()
+                            .bodyToMono(String.class))
+                    .subscribe(tuple2 -> log.info("RESPONSE: {}", tuple2));
         } catch (Exception e) {
-            return Flux.empty();
+            log.error("ERROR:{}", e.getMessage());
         }
     }
 
